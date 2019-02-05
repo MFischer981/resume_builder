91588c1f8e6a8a31ed87201fa217d70fbe845a77
Micah Fischer <mfischer981@west-mec.org>
2019-02-05 12:29:49 -0700

    Added certification text
    Added certification section. Created checkbox for current employment.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+function printPage() {
+    window.print();
+}
+
 var firstName
 var lastName
 var streetAddress
@@ -6,7 +10,6 @@ var locationState
 var zipCode
 var emailAddress
 var phoneNumber
-
 
 function updateInfo() {
     // Get Name Values
@@ -48,9 +51,11 @@ var resumeSkill
 var resumeSkillDesc
 
 function addSkill() {
+    // Get Skill Value
     resumeSkill = document.getElementById('skillName').value;
     resumeSkillDesc = document.getElementById('skillDescription').value;
 
+    // Add Skill
     document.getElementById("resumeSkill").innerHTML +=
         '<li>' + '<b>' + resumeSkill + '</b> - ' + resumeSkillDesc + '</li>';
 }
@@ -72,6 +77,63 @@ function addJob() {
     positionEndYear = document.getElementById('jobEndYear').value;
     positionDesc = document.getElementById('jobDesc').value;
 
-    document.getElementById('resumeJob').innerHTML +=
-        '<li>' + '<b>' + positionName + '</b>' + ' (' + positionStartMonth + ' ' + positionStartYear + ' - ' + positionEndMonth + ' ' + positionEndYear + ')' + '<br>' + '<i>' + positionCompany + '</i><br>' + positionDesc + '</li>'
+    if (document.getElementById('currentJob').checked) {
+        document.getElementById('resumeJob').innerHTML +=
+            '<li>' + '<b>' + positionName + '</b>' + ' (' + positionStartMonth + ' ' + positionStartYear + ' - ' + 'Present)' + '<br>' + '<i>' + positionCompany + '</i><br>' + positionDesc + '</li>'
+    } else {
+        document.getElementById('resumeJob').innerHTML +=
+            '<li>' + '<b>' + positionName + '</b>' + ' (' + positionStartMonth + ' ' + positionStartYear + ' - ' + positionEndMonth + ' ' + positionEndYear + ')' + '<br>' + '<i>' + positionCompany + '</i><br>' + positionDesc + '</li>'
+    }
+
+}
+
+var educationType
+var educationField
+var educationCollege
+var educationGPA
+var educationStartMonth
+var educationStartYear
+var educationEndMonth
+var educationEndYear
+var educationDesc
+
+function addEducation() {
+    educationType = document.getElementById('degreeType').value;
+    educationField = document.getElementById('degreeField').value;
+    educationCollege = document.getElementById('degreeCollege').value;
+    educationGPA = document.getElementById('degreeGPA').value;
+
+    educationStartMonth = document.getElementById('degreeStartMonth').value;
+    educationStartYear = document.getElementById('degreeStartYear').value;
+
+    educationEndMonth = document.getElementById('degreeEndMonth').value;
+    educationEndYear = document.getElementById('degreeEndYear').value;
+    educationDesc = document.getElementById('degreeDesc').value;
+
+    if (document.getElementById('includeGPA').checked) {
+        document.getElementById('resumeEducation').innerHTML +=
+            '<li><b>' + educationCollege + '</b>' + ' (' + educationStartMonth + ' ' + educationEndYear + ' - ' + educationEndMonth + ' ' + educationEndYear + ')<br>' + educationType + ' ' + educationField + ' (' + educationGPA + ' GPA)<br>' + educationDesc + '</li>'
+    } else {
+        document.getElementById('resumeEducation').innerHTML +=
+            '<li><b>' + educationCollege + '</b>' + ' (' + educationStartMonth + ' ' + educationEndYear + ' - ' + educationEndMonth + ' ' + educationEndYear + ')<br>' + educationType + ' ' + educationField + '<br>' + educationDesc + '</li>';
+    }
+}
+
+var certifyName
+var certifyIssuer
+var certifyIssueMonth
+var certifyIssueYear
+var certifyExpireMonth
+var certifyExpireYear
+
+function addCertificate() {
+    certifyName = document.getElementById('certificateName').value;
+    certifyIssuer = document.getElementById('certificateIssuer').value;
+    certifyIssueMonth = document.getElementById('certificateIssueMonth').value;
+    certifyIssueYear = document.getElementById('certificateIssueYear').value;
+    certifyExpireMonth = document.getElementById('certificateExpireMonth').value;
+    certifyExpireYear = document.getElementById('certificateExpireYear').value;
+
+    document.getElementById('resumeCertification').innerHTML +=
+        '<li>' + certifyName + ', ' + certifyIssuer + '<br>' + '(' + certifyIssueMonth + ' ' + certifyIssueYear + ' - ' + certifyExpireMonth + ' ' + certifyExpireYear + ')</li>'
 }
